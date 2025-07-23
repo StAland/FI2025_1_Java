3553b43188e6d177caf822bc7075b6ffb3ae996a
StAland <steffen.aland@gmx.de>
2025-07-23 11:57:10 +0200

    Überladungen

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        int zahl = 1;
-        int inkrement = 1;
-        while(zahl < 1000){
-            if(zahl % 2 == 0){
-                zahl /= 2;
-                System.out.println(zahl);
-                continue;
-            }
-            zahl += inkrement;
-            System.out.println(zahl);
-            inkrement++;
-        }
-
-//        for(int i = 1; i <= 100; i++){
-//            if(i == 77){
-//                break;
-//            }
-//            if (i % 7 == 0){
-//                System.out.println("beep");
-//                continue;
-//            }
-//            System.out.println(i);
-//        }
-
-//        int[] zahlen = {10, 25, 235, 912, 23};
-//        int[] zahlen2 = {324, 12, 123, 3443};
-//        int summe = 0;
-//        for (int zahl : zahlen2){
-//            summe += zahl;
-//        }
-//        System.out.println(summe);
+        double taxes = berechneSteuer(1000.0, 0.17);
+        System.out.println(taxes);
+        taxes = berechneSteuer(1000.0);
+        System.out.println(taxes);
     }
+
+    public static double berechneSteuer(double amount, double tax){
+        double taxes = amount * tax;
+        // Hier kommt noch ne ganz tolle Überprüfung rein
+        return taxes;
+    }
+
+    public  static double berechneSteuer(double amount){
+        return berechneSteuer(amount, 0.19);
+    }
+
 }
 
