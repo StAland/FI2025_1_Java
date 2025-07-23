f6712f134ba596439edb4dfc8aa9179ca57f3134
StAland <steffen.aland@gmx.de>
2025-07-23 13:44:07 +0200

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-//        long fac = fact(20);
-//        System.out.println(fac);
-        double ergebnis = me(50.0);
-        System.out.println(ergebnis);
-    }
-
-    public static double me(double number){
-        if (number < 0){
-            return 1;
-        }
-        return me(number-1) + me(number -2);
-    }
-
-    public static long fact(long number){
-
-//        long ergebnis = 1;                    Iterativ statt rekursiv
-//        for (long i = 2; i <= number; i++){
-//            ergebnis *= i;
-//        }
-
-        if (number < 0){
-            return -1;
-        }
-        if (number == 0 || number == 1){
-            return 1;
-        }
-        long ergebnis = fact(number - 1);
-        return number * ergebnis;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte gib eine Zahle ein: ");
+        int zahl = scanner.nextInt();
+        System.out.println(zahl);
+        scanner.close();
     }
 }
 
